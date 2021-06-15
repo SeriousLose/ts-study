d4285cd8deb39dde3e2e8b8bc2d3ce36a00d5272
helongfei <643756132@qq.com>
2021-06-15 15:59:06 +0800

    <feature> 学习tsconfig

---
+++ learnTsconfig/src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+function getVersion(version) {
+    if (version === void 0) { version = "1.0.0"; }
+    return version;
+}
+console.log(getVersion("1.0.1"));
+//# sourceMappingURL=index.js.map
---
+++ learnTsconfig/src/index.ts
@@ -0,0 +1,6 @@
+// 返回当前版本号
+function getVersion(version:string = "1.0.0"): string{
+  return version;
+}
+
+console.log(getVersion("1.0.1"))
